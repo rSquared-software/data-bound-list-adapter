a60d8b787a0cfc66036d1f41012c53cbaae96315
orlikraf <orlik.raf@gmail.com>
2019-07-02 14:58:58 +0200

    updated example

--- app/src/main/java/rsquared/software/example/BindingAdapters.kt
+++ app/src/main/java/rsquared/software/example/BindingAdapters.kt
@@ -1,9 +1,14 @@
 package rsquared.software.example
 
+import android.graphics.drawable.Animatable
+import android.graphics.drawable.Animatable2
+import android.graphics.drawable.Drawable
+import android.os.Build
 import android.view.View
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import androidx.databinding.ObservableBoolean
+import software.rsquared.logextensions.logWarning
 
 @BindingAdapter("iconRes")
 fun ImageView.bindIconRes(res: Int?) {
@@ -12,8 +17,8 @@ fun ImageView.bindIconRes(res: Int?) {
     } ?: setImageDrawable(null)
 }
 
-@BindingAdapter("toggleableBoolean")
-fun View.bindToggleBoolean(target: ObservableBoolean?) {
+@BindingAdapter("onClickToggle")
+fun View.bindOnClickToggleBoolean(target: ObservableBoolean?) {
     if (target == null) {
         setOnClickListener(null)
     } else {
@@ -23,7 +28,20 @@ fun View.bindToggleBoolean(target: ObservableBoolean?) {
     }
 }
 
-@BindingAdapter("positiveRotation", "rotateIf", requireAll = false)
-fun View.bindRotation(positive: Float?, rotateIf: Boolean?) {
-    rotation = if (rotateIf == true) positive ?: 180f else 0f
+@BindingAdapter("animateTrue", "animateFalse", "animateTrigger")
+fun ImageView.bindAnimatedVector(animTrue: Drawable, animFalse: Drawable, animate: Boolean?) {
+    animate ?: return
+    if (animate == true)
+        setImageDrawable(animTrue)
+    else
+        setImageDrawable(animFalse)
+
+    when (val d = drawable) {
+        is Animatable -> d.start()
+        else -> if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && d is Animatable2)
+            d.start()
+        else
+            logWarning("drawable is not Animatable")
+
+    }
 }
