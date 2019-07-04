c9d8abe172ad75a9ab0d0807341e7efe55e1b153
orlikraf <orlik.raf@gmail.com>
2019-07-04 16:51:10 +0200

    peculiar case of unstable ids

--- app/src/main/java/rsquared/software/example/group/GroupViewModel.kt
+++ app/src/main/java/rsquared/software/example/group/GroupViewModel.kt
@@ -6,7 +6,7 @@ import software.rsquared.logextensions.logVerbose
 class GroupViewModel : ViewModel() {
 
     val eventList = listOf(
-        Event(id = 10, parentId = 0, name = "Romance Dawn"),
+        Event(id = 11, parentId = 0, name = "Romance Dawn"),
 
         EventsHeader(id = 0, groupId = 1, name = "Parties"),
         Event(id = 1, parentId = 1, name = "Let's eat party"),
@@ -14,7 +14,7 @@ class GroupViewModel : ViewModel() {
 
         EventsHeader(id = 3, groupId = 2, parentId = 1, name = "Epic parties"),
         Event(id = 4, parentId = 2, name = "Enies Lobby"),
-        Event(id = 11, parentId = 2, name = "Welcome to Wano"),
+        Event(id = 10, parentId = 2, name = "Welcome to Wano"),
 
         EventsHeader(id = 5, groupId = 3, name = "Birthdays"),
         Event(id = 6, parentId = 3, name = "Robin's birthday"),
@@ -24,7 +24,7 @@ class GroupViewModel : ViewModel() {
     )
 
     val eventListCopy = listOf(
-        Event(id = 10, parentId = 0, name = "Romance Dawn"),
+        Event(id = 11, parentId = 0, name = "Romance Dawn"),
 
         EventsHeader(id = 0, groupId = 1, name = "Parties"),
         Event(id = 1, parentId = 1, name = "Let's eat party"),
@@ -32,13 +32,13 @@ class GroupViewModel : ViewModel() {
 
         EventsHeader(id = 3, groupId = 2, parentId = 1, name = "Epic parties"),
         Event(id = 4, parentId = 2, name = "Enies Lobby"),
-        Event(id = 11, parentId = 2, name = "Welcome to Wano"),
+        Event(id = 5, parentId = 2, name = "Welcome to Wano"),
 
-        EventsHeader(id = 5, groupId = 3, name = "Birthdays"),
-        Event(id = 6, parentId = 3, name = "Robin's birthday"),
+        EventsHeader(id = 6, groupId = 3, name = "Birthdays"),
+        Event(id = 7, parentId = 3, name = "Robin's birthday"),
         Event(id = 8, parentId = 3, name = "Nami's birthday"),
-        Event(id = 7, parentId = 3, name = "Franky's birthday"),
-        Event(id = 9, parentId = 3, name = "Zorro's birthday")
+        Event(id = 9, parentId = 3, name = "Franky's birthday"),
+        Event(id = 10, parentId = 3, name = "Zorro's birthday")
     )
 
     var lastReturned = eventList
